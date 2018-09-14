e6cf695b9aece9e523b218fe459b6c553234a017
yfsoftcom <yfsoftcom@126.com>
2018-09-14 16:54:27 +0800

    catch encoder

--- source/kit.js
+++ source/kit.js
@@ -12,13 +12,18 @@ exports.decoder = hex => {
     if(!Buffer.isBuffer(hex)){
         return { header: { uid: -1, pid: -1, nb: -1, sid: -1}, payload: Buffer.from([0,0,0,0])}
     }
-    const uid = hex.readUIntBE(0, 4);
-    const pid = hex.readUIntBE(4, 4);
-    let nb = hex.toString('hex', 8, 8 + 8); // it should be a utf8 string with 8 bytes
-    nb = nb.substring(1)
-    // const sid = hex.readUIntBE('utf8', 23, 4);
-    const sid = hex.toString('hex', 16, 16 + 4);
-    const data = hex.slice(16);
+    try{
+        const uid = hex.readUIntBE(0, 4);
+        const pid = hex.readUIntBE(4, 4);
+        let nb = hex.toString('hex', 8, 8 + 8); // it should be a utf8 string with 8 bytes
+        nb = nb.substring(1)
+        // const sid = hex.readUIntBE('utf8', 23, 4);
+        const sid = hex.toString('hex', 16, 16 + 4);
+        const data = hex.slice(16);
 
-    return { header: { uid, pid, nb, sid }, payload: data.toString('hex') }
+        return { header: { uid, pid, nb, sid }, payload: data.toString('hex') }
+    }catch(e){
+        console.error(e);
+        return { header: { uid: -1, pid: -1, nb: -1, sid: -1}, payload: Buffer.from([0,0,0,0])}
+    }
 }
