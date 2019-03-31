146181b1e969c56674996a6773d2d5047ca22658
yfsoftcom <yfsoftcom@126.com>
2019-03-31 02:19:04 -0400

    catch the syntx error

--- source/webhook.js
+++ source/webhook.js
@@ -8,17 +8,21 @@ const { decoder } = require('./kit.js');
 
 const createWebhook = fpm => {
   fpm.subscribe(`#webhook/tianyi/notify`, (topic, message) => {
-    // console.log(topic, message);
-    debug('%o, %O', topic, message);
-    const { deviceId, services } = message;
-    debug('typeof: %s, data: %s', typeof(services.data), services.data );
-    fpm.execute('mqtt.publish', {
-      topic: `$d2s/u13/p0/tianyi`,
-      payload: { deviceId, data: services.data },
-    })
-    .catch(error => {
-      debug('ERROR: %O', error)
-    })
+    try {
+      debug('%o, %O', topic, message);
+      const { deviceId, services } = message;
+      debug('typeof: %s, data: %s', typeof(services.data), services.data );
+      fpm.execute('mqtt.publish', {
+        topic: `$d2s/u13/p0/tianyi`,
+        payload: { deviceId, data: services.data },
+      })
+      .catch(error => {
+        debug('ERROR: %O', error)
+      })  
+    } catch (error) {
+      debug('syntx ERROR: %O:', error)
+    }
+    
     // fpm.publish('#tianyi/notify', message);
   })
 }
