253462aaee01a8cc625aa4b39e081bd24aeba24f
yfsoftcom <yfsoftcom@126.com>
2019-03-31 02:49:53 -0400

    test

--- source/webhook.js
+++ source/webhook.js
@@ -11,7 +11,7 @@ const createWebhook = fpm => {
     try {
       debug('%o, %O', topic, message);
       const { deviceId, service } = message;
-      debug('typeof: %s, data: %s', typeof(service.data.Data), Buffer.from(service.data.Data, 'hex').toString() );
+      debug('typeof: %s, data: %s', typeof(service.data.Data), Buffer.from(service.data.Data).toString('hex') );
       fpm.execute('mqttclient.publish', {
         topic: `$d2s/u13/p0/tianyi`,
         payload: { deviceId, data: service.data },
