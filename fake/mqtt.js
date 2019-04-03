85264e50803ab6e9dd1ed61a5a51b296bd5ccc39
mrstack <mrstack@163.com>
2019-04-03 10:15:17 +0800

    add test script

--- fake/mqtt.js
+++ fake/mqtt.js
@@ -8,7 +8,7 @@ const client = mqtt.connect(`mqtt://${host}:${port}`, config);
 // 	client.publish('$s2d/tcp/broadcast', JSON.stringify({ message: '0102030405', channel: '', ids: ''}), { qos: 1, retain: true});
 // },10000);
 
-client.publish('$s2d/nb/yiyuan/push', '141c0c85-24ca-4d2d-bf57-7e45591a3b88|05001308fffefdfc01000000', { qos: 1, retain: true});
+client.publish('$s2d/nb/yiyuan/push', '14debc83-2870-4d6e-a816-72a58de650f5|05001328fffefdfc010000000100000001000000010000000100000001000000010000000100000001000000', { qos: 1, retain: true});
 
 // client.subscribe('$s2d/nb/youren/push');
 client.on('message', (topic, payload) => {
