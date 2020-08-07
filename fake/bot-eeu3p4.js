eb57cfe2b3df38c261c51ff7f02ebdf1bf70d981
yfsoftcom <yfsoftcom@126.com>
2020-08-07 08:40:50 +0800

    test env

--- fake/bot-eeu3p4.js
+++ fake/bot-eeu3p4.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const _ = require('lodash');
 
-const LOCAL_HOST = 'www.ruichen.top';
+const LOCAL_HOST = 'localhost';
 
 const PORT = 5001;
 
@@ -18,7 +18,7 @@ const send = ( client, data ) => {
         }
         return
     }
-    data = data || Buffer.from(`ee00000300000004ffffffff010000${Buffer.from(JSON.stringify({a:2})).toString('hex')}`, 'hex')
+    data = data || Buffer.from(`ee00000300000004ffffffff010000${Buffer.from(JSON.stringify({ "sn_id":"abcdefgh", "opcode": 1001, "device_type": 20, "min_wind_dir": 0, "avg_wind_dir": 0, "max_wind_dir": 0, "min_wind_speed": 0, "avg_wind_speed": 0, "max_wind_speed": 2, "temp": 253, "humidity": 597, "pressure": 10087, "rainfall": 0, "radiation": 0, "u-rays": 0, "noise": 562, "pm2.5": 0, "pm10": 0 })).toString('hex')}`, 'hex')
     console.info(data.toString('hex'), data)
     client.write(data);
 }
